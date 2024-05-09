9458142a4727a3c7f55cfc230c8f0ae295c1b254
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2024-05-09 15:22:19 +0100

    feat: envio de email

---
+++ assets/js/email.service.js
@@ -0,0 +1,95 @@
+class FormSubmit {
+    constructor(settings) {
+        this.settings = settings;
+        console.log(document.getElementById(settings.form));
+        this.form = document.getElementById(settings.form);
+        this.formButton = document.getElementById(settings.button);
+        if(this.form){
+            this.url = this.form.getAttribute('action');
+        }
+    }
+
+    setAlert(classValue, message){
+        const alerta = document.getElementById('alerta');
+        alerta.className = classValue;
+        alerta.innerHTML = message;
+        setTimeout(()=>{
+            alerta.className = 'd-none';
+            alerta.innerHTML = '';
+        },3000);
+    }
+
+    getFormData(){
+      const formObject = {};
+      const fields = this.form.querySelectorAll('[name]');
+      fields.forEach((field) => {
+            formObject[field.getAttribute('name')] = field.value;  
+      });
+      return formObject;
+    }
+
+    async sendForm(e){
+        this.onSubmmited(e);
+        try{
+            await fetch(this.url, {
+                method: 'POST',
+                headers:{
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify(this.getFormData())
+            })
+            this.displaySucess();
+            this.resetButton(e);
+            this.resetForm();
+        } catch(error){
+            this.displayError();
+        }
+        
+    }
+
+    onSubmmited(e){
+        e.preventDefault();
+        e.target.disabled = true;
+        e.target.innerText = '';
+        const loading = `<span class="spinner-grow spinner-grow-sm" aria-hidden="true"></span>
+        <span role="status">Enviando...</span>`; 
+        e.target.innerHTML = loading;
+    }
+
+    resetButton(event){       
+        event.target.disabled = false;
+        event.target.innerText = 'ENVIE SUA MENSAGEM';
+    }
+
+    resetForm(){
+        const fields = this.form.querySelectorAll('[name]');
+        fields.forEach((field) => {
+            field.value = '';
+        });
+    }
+
+    displaySucess(){
+        this.setAlert(this.settings.success, 'Formulário enviado com sucesso');
+    }
+
+    displayError(){
+        this.setAlert(this.settings.error, 'Erro ao enviar formulário');
+    }
+
+     init(){
+        if(this.form){
+            this.formButton.addEventListener('click', async (e) => await this.sendForm(e));
+        }
+        return this;
+    }
+}
+
+const formSubmit = new FormSubmit({
+    form: ['formulario'],
+    button: ['buttonEnviar'],
+    success: "alert alert-success",
+    error: "alert alert-danger"
+});
+
+formSubmit.init();

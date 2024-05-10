218e7b3773f2195f491deeffa99603aae5cba7cc
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2024-05-10 01:18:43 +0100

    feat: logo parceiros e clientes, correção de erros ortografios e responsividade e correção de carousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
         loop:true,
         nav:true,
         autoplay:true,
+        autoplayTimeout: 10000, 
         autoplaySpeed:1000,
         smartSpeed:1500,
         autoplayHoverPause:true,
